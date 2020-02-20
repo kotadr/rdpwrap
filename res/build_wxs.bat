f65d55eb0f565c003ba4c42ebdf076bd2a8da373
kotadr <57463717+kotadr@users.noreply.github.com>
2020-02-20 18:55:34 +0200

    DCC output directory

--- res/build_wxs.bat
+++ res/build_wxs.bat
@@ -1,3 +1,3 @@
-@echo off
-"%ProgramFiles%\WiX Toolset v3.11\bin\candle" RDPWInst.wxs
-"%ProgramFiles%\WiX Toolset v3.11\bin\light" RDPWInst.wixobj
+@echo on
+"%ProgramFiles(x86)%\WiX Toolset v3.11\bin\candle" RDPWInst.wxs
+"%ProgramFiles(x86)%\WiX Toolset v3.11\bin\light" RDPWInst.wixobj
