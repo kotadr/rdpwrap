ad16199334e9623f56d8b6eb7103c71908eebf3d
kotadr <57463717+kotadr@users.noreply.github.com>
2020-02-20 18:06:30 +0200

    Updare url

--- src-installer/RDPWInst.dpr
+++ src-installer/RDPWInst.dpr
@@ -618,7 +618,7 @@ end;
 function GitINIFile(var Content: String): Boolean;
 const
 //  URL = 'https://raw.githubusercontent.com/stascorp/rdpwrap/master/res/rdpwrap.ini';
-  URL = 'https://raw.githubusercontent.com/kotadr/rdpwrap/master/rdpwrap.ini';
+  URL = 'https://raw.githubusercontent.com/kotadr/rdpwrap/master/res/rdpwrap.ini';
 var
   NetHandle: HINTERNET;
   UrlHandle: HINTERNET;
