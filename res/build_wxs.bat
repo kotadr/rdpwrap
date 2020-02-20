ade369ea8ce8a97e734a00ef5a30402da6bb7f0e
kotadr <57463717+kotadr@users.noreply.github.com>
2020-02-20 21:58:39 +0200

    Update

--- res/build_wxs.bat
+++ res/build_wxs.bat
@@ -1,3 +1,3 @@
 @echo on
 "%ProgramFiles(x86)%\WiX Toolset v3.11\bin\candle" RDPWInst.wxs
-"%ProgramFiles(x86)%\WiX Toolset v3.11\bin\light" RDPWInst.wixobj
+"%ProgramFiles(x86)%\WiX Toolset v3.11\bin\light" RDPWInst.wixobj -o ..\bin\RDPWInst
